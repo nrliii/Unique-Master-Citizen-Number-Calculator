aaf7e983885fd2b4f79a6feee591bf7d3fcaa638
prgicnikola <prgicnikola@outlook.com>
2023-10-24 21:20:55 +0200

    Add project files.

---
+++ JMBGRework/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace JMBGRework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.izracunajButton = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBoxM = new System.Windows.Forms.CheckBox();
+            this.checkBoxZ = new System.Windows.Forms.CheckBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.drzComboBox = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.regComboBox = new System.Windows.Forms.ComboBox();
+            this.labelProvera = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // izracunajButton
+            // 
+            this.izracunajButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.izracunajButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.izracunajButton.Location = new System.Drawing.Point(20, 218);
+            this.izracunajButton.Name = "izracunajButton";
+            this.izracunajButton.Size = new System.Drawing.Size(182, 39);
+            this.izracunajButton.TabIndex = 0;
+            this.izracunajButton.Text = "Izracunaj";
+            this.izracunajButton.UseVisualStyleBackColor = true;
+            this.izracunajButton.Click += new System.EventHandler(this.izracunajButton_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(20, 192);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(182, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // checkBoxM
+            // 
+            this.checkBoxM.AutoSize = true;
+            this.checkBoxM.Location = new System.Drawing.Point(20, 158);
+            this.checkBoxM.Name = "checkBoxM";
+            this.checkBoxM.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxM.TabIndex = 2;
+            this.checkBoxM.Text = "Musko";
+            this.checkBoxM.UseVisualStyleBackColor = true;
+            this.checkBoxM.CheckedChanged += new System.EventHandler(this.checkBoxM_CheckedChanged);
+            // 
+            // checkBoxZ
+            // 
+            this.checkBoxZ.AutoSize = true;
+            this.checkBoxZ.Location = new System.Drawing.Point(140, 158);
+            this.checkBoxZ.Name = "checkBoxZ";
+            this.checkBoxZ.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxZ.TabIndex = 3;
+            this.checkBoxZ.Text = "Zensko";
+            this.checkBoxZ.UseVisualStyleBackColor = true;
+            this.checkBoxZ.CheckedChanged += new System.EventHandler(this.checkBoxZ_CheckedChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(20, 263);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(182, 20);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // drzComboBox
+            // 
+            this.drzComboBox.FormattingEnabled = true;
+            this.drzComboBox.Location = new System.Drawing.Point(20, 73);
+            this.drzComboBox.Name = "drzComboBox";
+            this.drzComboBox.Size = new System.Drawing.Size(182, 21);
+            this.drzComboBox.TabIndex = 5;
+            this.drzComboBox.SelectedIndexChanged += new System.EventHandler(this.drzComboBox_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.buttonSelect.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSelect.Location = new System.Drawing.Point(20, 12);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(182, 39);
+            this.buttonSelect.TabIndex = 7;
+            this.buttonSelect.Text = "Selektuj";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // regComboBox
+            // 
+            this.regComboBox.FormattingEnabled = true;
+            this.regComboBox.Location = new System.Drawing.Point(20, 113);
+            this.regComboBox.Name = "regComboBox";
+            this.regComboBox.Size = new System.Drawing.Size(182, 21);
+            this.regComboBox.TabIndex = 8;
+            this.regComboBox.SelectedIndexChanged += new System.EventHandler(this.regComboBox_SelectedIndexChanged);
+            // 
+            // labelProvera
+            // 
+            this.labelProvera.AutoSize = true;
+            this.labelProvera.Location = new System.Drawing.Point(17, 57);
+            this.labelProvera.Name = "labelProvera";
+            this.labelProvera.Size = new System.Drawing.Size(0, 13);
+            this.labelProvera.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 310);
+            this.Controls.Add(this.labelProvera);
+            this.Controls.Add(this.regComboBox);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.drzComboBox);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.checkBoxZ);
+            this.Controls.Add(this.checkBoxM);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.izracunajButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "JMBG Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button izracunajButton;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBoxM;
+        private System.Windows.Forms.CheckBox checkBoxZ;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.ComboBox drzComboBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.ComboBox regComboBox;
+        private System.Windows.Forms.Label labelProvera;
+    }
+}
+
---
+++ JMBGRework/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace JMBGRework
+{
+    public partial class Form1 : Form
+    {
+        private string bazaPath;
+        private string uid;
+        private string cifra;
+        private string mestorodjenja;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void izracunajButton_Click(object sender, EventArgs e)
+        {
+            DateTime datumRodjenja = dateTimePicker1.Value;
+            int godinaZadnjeTri = datumRodjenja.Year % 1000;
+            string datum = datumRodjenja.ToString("ddMM") + godinaZadnjeTri.ToString("D3");
+            string pol;
+            if (drzComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Izaberite drzavu/region.");
+                return;
+            }
+            if (regComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Izaberite region.");
+                return;
+            }
+
+            if (checkBoxM.Checked)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(1, 500);
+                string randomString = randomNumber.ToString("D3");
+                pol = randomString;
+            }
+            else if (checkBoxZ.Checked)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(501, 1000);
+                string randomString = randomNumber.ToString("D3");
+                pol = randomString;
+            }
+            else
+            {
+                MessageBox.Show("Izaberite pol.");
+                return;
+            }
+            string jmbg = datum + mestorodjenja + pol;
+            int checksum = IzracunajChecksum(jmbg);
+            string checksumfilterovan = "";
+            if (checksum >= 1 && checksum <= 9)
+            { 
+                checksumfilterovan = checksum.ToString();
+            }
+            else if (checksum == 10 || checksum == 11)
+            {
+            checksumfilterovan = "0"; 
+            }
+
+            textBoxOutput.Text = jmbg + checksumfilterovan;
+        }
+        private int IzracunajChecksum(string jmbg)
+        {
+            int a = int.Parse(jmbg[0].ToString());
+            int b = int.Parse(jmbg[1].ToString());
+            int c = int.Parse(jmbg[2].ToString());
+            int d = int.Parse(jmbg[3].ToString());
+            int e = int.Parse(jmbg[4].ToString());
+            int f = int.Parse(jmbg[5].ToString());
+            int g = int.Parse(jmbg[6].ToString());
+            int h = int.Parse(jmbg[7].ToString());
+            int i = int.Parse(jmbg[8].ToString());
+            int j = int.Parse(jmbg[9].ToString());
+            int k = int.Parse(jmbg[10].ToString());
+            int l = int.Parse(jmbg[11].ToString());
+
+            int checksum = 11 - ((7 * (a + g) + 6 * (b + h) + 5 * (c + i) + 4 * (d + j) + 3 * (e + k) + 2 * (f + l)) % 11);
+            return checksum;
+        }
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog otvoriBazu = new OpenFileDialog();
+
+            otvoriBazu.Title = "Selektuj bazu";
+            otvoriBazu.Filter = "Access Baza fajl|*.accdb";
+            otvoriBazu.FilterIndex = 1;
+
+            DialogResult result = otvoriBazu.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                bazaPath = otvoriBazu.FileName;
+                UcitajRegioneBaza(bazaPath);
+                buttonSelect.Text = "BAZA UCITANA!";
+                buttonSelect.ForeColor = Color.Red;
+                buttonSelect.Font = new Font(buttonSelect.Font, FontStyle.Bold);
+                buttonSelect.UseVisualStyleBackColor = false;
+            }
+        }
+        private void UcitajRegioneBaza(string bazaPath)
+        {
+            string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={bazaPath};";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    DataTable schema = connection.GetSchema("Tables");
+                    foreach (DataRow row in schema.Rows)
+                    {
+                        string izabranaTabela = (string)row["TABLE_NAME"];
+                        if (!izabranaTabela.StartsWith("MSys"))
+                        {
+                            drzComboBox.Items.Add(izabranaTabela);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void drzComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string izabranaTabela = drzComboBox.SelectedItem.ToString();
+            List<string> regionData = RegionIzTabele(izabranaTabela, bazaPath);
+            regComboBox.Items.Clear();
+            foreach (string region in regionData)
+            {
+                regComboBox.Items.Add(region);
+            }
+        }
+        private void regComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selektovanRegion = regComboBox.SelectedItem.ToString();
+            string izabranaTabela = drzComboBox.SelectedItem.ToString();
+            string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={bazaPath};";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+                try
+            {
+                connection.Open();
+                string query = $"SELECT UID, Cifra FROM {izabranaTabela} WHERE Region = @region";
+                OleDbCommand cmd = new OleDbCommand(query, connection);
+                cmd.Parameters.AddWithValue("@region", selektovanRegion);
+
+                OleDbDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    uid = reader["UID"].ToString();
+                    cifra = reader["Cifra"].ToString();
+                    mestorodjenja = uid + cifra;
+                }
+                else
+                {
+                        MessageBox.Show("Error Ne pronadjeno");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+        private List<string> RegionIzTabele(string tableName, string bazaPath)
+        {
+            List<string> regionData = new List<string>();
+
+            string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={bazaPath};";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = $"SELECT Region FROM {tableName}";
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    {
+                        using (OleDbDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string region = reader["Region"].ToString();
+                                regionData.Add(region);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return regionData;
+            }
+        }
+
+        private void checkBoxM_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxM.Checked)
+            {
+                checkBoxZ.Checked = false;
+            }
+        }
+        private void checkBoxZ_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxZ.Checked)
+            {
+                checkBoxM.Checked = false;
+            }
+        }
+    }
+}
